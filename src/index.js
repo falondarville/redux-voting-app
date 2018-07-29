fb2e4a3a82f138d556c0255953ba578f78f81e47
Falon Darville <falondarville@gmail.com>
2018-07-28 17:21:09 -0700

    add actions, reducers, and set up store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
+import myApp from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// in the createStore function, which is a built-in Redux function, we hand it the reducers, which are imported to this file as "myApp"
+let store = createStore(myApp);
+
+// ReactDOM.render comes on its own, but we place it inside our own render function and call this render function at the end of the file. 
+// we do this because we first have to subscribe to the store. That way, each time the store changes, we are calling the render function. 
+function render () {
+	ReactDOM.render(
+		<App />, 
+		document.getElementById('root'));
+		registerServiceWorker();
+}
+
+// subscribe to the store
+store.subscribe(render);
+
+// we have to call this function here so that it will run at the start. 
+render();
---
+++ src/reducers/index.js
@@ -0,0 +1,27 @@
+// create the inital state when the app is starting up
+const initalState = {
+	react: 0,
+	angular: 0,
+	vuejs: 0
+}
+
+// reducer function which has two arguments
+// initialState is set at the default for the state argument
+export default (state = initalState, action) =>{
+	switch (action.type){
+		case: 'VOTE_REACT':
+			return Object.assign({}, state, {
+				react: state.react + 1
+			})
+		case: 'VOTE_ANGULAR':
+			return Object.assign({}, state, {
+				angular: state.angular + 1
+			})
+		case: 'VOTE_VUEJS':
+			return Object.assign({}, state, {
+				vuejs: state.vuejs + 1
+			})
+		default:
+			return state
+	}
+}
