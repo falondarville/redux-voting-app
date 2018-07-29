7fd4d252430006de6dfe9fff0c1e8c5e3434eb95
Falon Darville <falondarville@gmail.com>
2018-07-28 17:35:49 -0700

    fix switch statement

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,15 +9,15 @@ const initalState = {
 // initialState is set at the default for the state argument
 export default (state = initalState, action) =>{
 	switch (action.type){
-		case: 'VOTE_REACT':
+		case 'VOTE_REACT':
 			return Object.assign({}, state, {
 				react: state.react + 1
 			})
-		case: 'VOTE_ANGULAR':
+		case 'VOTE_ANGULAR':
 			return Object.assign({}, state, {
 				angular: state.angular + 1
 			})
-		case: 'VOTE_VUEJS':
+		case 'VOTE_VUEJS':
 			return Object.assign({}, state, {
 				vuejs: state.vuejs + 1
 			})
